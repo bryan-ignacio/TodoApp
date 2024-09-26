19060493d0d237b13582fddc78589c76270b88d7
Bryan <bryan-ignacio@outlook.com>
2024-09-26 01:41:17 -0600

    itemTask ui completed.

--- app/src/main/java/com/androidgt/todoapp/MainActivity.kt
+++ app/src/main/java/com/androidgt/todoapp/MainActivity.kt
@@ -5,7 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.androidgt.todoapp.addtasks.ui.TasksScreen
 import com.androidgt.todoapp.addtasks.ui.TasksViewModel
 import com.androidgt.todoapp.ui.theme.TodoAppTheme
@@ -21,7 +28,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoAppTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize().background(color = Color.LightGray)
+                ) {
                     TasksScreen(taskViewModel)
+                }
             }
         }
     }
--- app/src/main/java/com/androidgt/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/androidgt/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,18 +1,23 @@
 package com.androidgt.todoapp.addtasks.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -28,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -41,11 +47,46 @@ fun TasksScreen(taskViewModel: TasksViewModel) {
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .padding(top = 40.dp)
             .background(color = Color.LightGray)
     )
     {
-        AddTaskDialog(show = showDialog, onDismiss = { taskViewModel.onDialogClose() }, onTaskAdded = {taskViewModel.onTaskCreated(it)})
-        FabDialog(modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp), taskViewModel)
+        AddTaskDialog(
+            show = showDialog,
+            onDismiss = { taskViewModel.onDialogClose() },
+            onTaskAdded = { taskViewModel.onTaskCreated(it) })
+        FabDialog(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp), taskViewModel
+        )
+        TasksList(taskViewModel)
+    }
+}
+
+@Composable
+fun TasksList(taskViewModel: TasksViewModel) {
+
+}
+
+@Composable
+fun ItemTask() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp, vertical = 10.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.White)
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween
+        ) {
+            Text(text = "Primera Tarea ", modifier = Modifier.padding(start = 12.dp))
+            Checkbox(checked = true, onCheckedChange = {})
+        }
     }
 }
 
@@ -74,7 +115,7 @@ fun AddTaskDialog(
                 shape = RoundedCornerShape(10.dp), // Esquinas redondeadas
                 color = Color.White,
                 tonalElevation = 4.dp,
-            ){
+            ) {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
