1c72502fabd0e98138fc5c0eb1cc18a2c9e1ad77
Bryan <bryan-ignacio@outlook.com>
2024-09-26 00:26:23 -0600

    dialog completed.

--- app/src/main/java/com/androidgt/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/androidgt/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,15 +1,23 @@
 package com.androidgt.todoapp.addtasks.ui
 
+import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,8 +27,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 
 @Composable
@@ -31,16 +42,19 @@ fun TasksScreen(taskViewModel: TasksViewModel) {
             .background(color = Color.LightGray)
     )
     {
-        FabDialog(modifier = Modifier
-            .align(Alignment.BottomEnd)
-            .padding(16.dp))
-
+        AddTaskDialog(show = true, onDismiss = {})
+        FabDialog(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        )
     }
 }
 
 @Composable
 fun FabDialog(modifier: Modifier) {
-    FloatingActionButton(onClick = {}, modifier = modifier) {
+    FloatingActionButton(onClick = {
+    }, modifier = modifier) {
         Icon(Icons.Default.Add, contentDescription = "Add")
     }
 }
@@ -55,11 +69,33 @@ fun AddTaskDialog(
 
     if (show) {
         Dialog(onDismissRequest = { onDismiss() }) {
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Agrega tu tarea")
-                TextField(value = myTask, onValueChange = {})
+
+            Surface(
+                shape = RoundedCornerShape(10.dp), // Esquinas redondeadas
+                color = Color.White,
+                tonalElevation = 4.dp,
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.White)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Agrega una tarea",
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    TextField(value = myTask, onValueChange = { myTask = it }, singleLine = true)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(onClick = { }, modifier = Modifier.fillMaxWidth(0.4f)) {
+                        Text(text = "Agregar")
+                    }
+                }
             }
         }
     }
